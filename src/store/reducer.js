c30ec82e41dfb7bbe174b345a515a5e16753a11e
Mindaugas Azubalis <mindaugas.azubalis@gmail.com>
2020-02-19 18:47:37 +0200

    now all works with react-redux

---
+++ src/store/reducer.js
@@ -0,0 +1,19 @@
+const initialState = {
+    age: 43
+};
+
+const reducer = (state = initialState, action) => {
+    const newState = {...state};
+
+    if(action.type === 'AGE_UP') {
+        newState.age++;
+    }
+
+    if(action.type === 'AGE_DOWN') {
+        newState.age--;
+    }    
+
+    return newState;
+}
+
+export default reducer;
